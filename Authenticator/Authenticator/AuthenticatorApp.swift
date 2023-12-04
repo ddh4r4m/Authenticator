6e635521e70a35aca69761d185d724ac3b9134be
Dharam Dhurandhar <dharamdhurandhar@Dharams-MacBook-Pro.local>
2023-12-04 22:27:16 +0530

    Project Setup

---
+++ Authenticator/Authenticator/AuthenticatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthenticatorApp.swift
+//  Authenticator
+//
+//  Created by Dharam Dhurandhar on 04/12/23.
+//
+
+import SwiftUI
+
+@main
+struct AuthenticatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
